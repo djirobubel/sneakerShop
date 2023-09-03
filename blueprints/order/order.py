ca946ae5564b60c87bf94f8d9a5d293b7388d37b
David <www.beljas007@gmail.com>
2023-09-03 14:14:08 +0300

    An exception for non-existent customer in 'POST' method has been created.

--- blueprints/order/order.py
+++ blueprints/order/order.py
@@ -29,30 +29,33 @@ def get_order(id):
 @order_bp.route('/orders', methods=['POST'])
 def create_order():
     with db.atomic() as transaction:
-        order = Order.create(customer_id=request.json['customerId'])
+        try:
+            order = Order.create(customer_id=request.json['customerId'])
+            Customer.get(Customer.id == order.customer_id)
 
-        items = request.json['items']
-        for item in items:
-            try:
-                new_product_item_id = item['productId']
-                new_product_size_id = item['sizeId']
-                Size.get(Size.id == new_product_size_id)
-                new_product_quantity = item['quantity']
-                product = Product.get(Product.id == new_product_item_id)
-                new_product_price = product.price * new_product_quantity
+            items = request.json['items']
+            for item in items:
+                try:
+                    new_product_item_id = item['productId']
+                    new_product_size_id = item['sizeId']
+                    Size.get(Size.id == new_product_size_id)
+                    new_product_quantity = item['quantity']
+                    product = Product.get(Product.id == new_product_item_id)
+                    new_product_price = product.price * new_product_quantity
 
-                OrderItem.create(order_id=order.id, product_id=new_product_item_id,
-                                 size_id=new_product_size_id,
-                                 quantity=new_product_quantity, price=new_product_price)
+                    OrderItem.create(order_id=order.id, product_id=new_product_item_id,
+                                     size_id=new_product_size_id,
+                                     quantity=new_product_quantity, price=new_product_price)
 
-            except Product.DoesNotExist:
-                transaction.rollback()
-                return {'error': 'product not found'}, 404
-            except Size.DoesNotExist:
-                transaction.rollback()
-                return {'error': 'size not found'}, 404
+                    return {'id': order.id}
 
-        return {'id': order.id}
+                except Product.DoesNotExist:
+                    transaction.rollback()
+                    return {'error': 'product not found'}, 404
+                except Size.DoesNotExist:
+                    transaction.rollback()
+                    return {'error': 'size not found'}, 404
 
-
-
+        except Customer.DoesNotExist:
+            transaction.rollback()
+            return {'error': 'customer not found'}, 404
